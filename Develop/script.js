abb2f20e94f4db80845c327f6fcd6261a11ab4c9
Yazan Deek <ydeek91@yahoo.com>
2020-09-11 20:07:04 -0400

    updated script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,167 +1,32 @@
-//display the current day and time on the top of the schedule using moment.js date and time format
 
 $("#currentDay").text(moment().format('MMMM Do YYYY'));
 
 
+$(".container").on("click", ".hour-block", function () {
+    const hour = $(this).attr("hour")
+    const id = `#textarea-${hour}`
+    const text = $(id).val()
+    localStorage.setItem(hour, text)
+})
 
-// how to apply the colors to past, present, and future ?!! 
-//create function to compare current time with time blocks
-function checkTime() {
-    //for 9am 
-    var timeBlock = moment().startOf('day').add(9, "hours")
-    //adjusting current time to the hour 
-    var currentTime = moment().startOf("hour")
-    //adding timeblock 1 if else statement
-    if (currentTime > timeBlock) {
-        $("#text-entry1").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry1").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry1").addclass("present");
-    }
+const get_rows = () => {
+    const now = new Date();
+    const hours = now.getHours()
+    for (let i = 9; i < 18; i++) {
+        let time = "present"
+        if (i < hours) {
+            time = "past"
+        } else if (i > hours) {
+            time = " future"
+        }
 
-    var timeBlock = moment().startOf('day').add(10, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry2").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry2").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry2").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(10, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry3").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry3").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry3").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(11, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry4").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry4").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry4").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(12, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry5").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry5").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry5").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(1, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry6").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry6").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry6").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(10, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry7").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry7").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry7").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(10, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry8").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry8").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry8").addclass("present");
-    }
-    var timeBlock = moment().startOf('day').add(10, "hours")
-    var currentTime = moment().startOf("hour")
-    if (currentTime > timeBlock) {
-        $("#text-entry9").addClass("past");
-    }
-    else if (currentTime.thePrevious(timeblock)) {
-        $("#text-entry9").addclass("future");
-    }
-    else if (currentTime.theSame(timeblock)) {
-        $("#text-entry9").addclass("present");
+        $(".container").append(`
+    <div id="calendar-row${i}" class="row justify-content-md-center border-top border-bottom calendar-row">
+        <div id="time${i}" class="col-md-1 ${time}">${i <= 12 ? `${i} AM` : `${i - 12} PM`}</div>
+        <textarea rows="3" id="textarea-${i}" class="col-md-10"></textarea>
+        <button id="save2" class="col-md-1 hour-block" hour=${i}><i class="far fa-save"></i></button>
+      </div>`)
     }
 }
-checkTime();
 
-//make written text saved in local storage using jQuery 
-//Change text of relavent timeblock to task here 
-var timeBlocks = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
-
-for (var i = 0; i < timeBlocks.length; i++) {
-    var task = localStorage.getItem(timeBlocks[i]);
-    $('#text-entry' + (i + 1)).text(task)
-}
-
-
-
-
-//timeblock: "9am" to "5pm"
-//task: any string 
-function saveTask(timeBlock, task) {
-    localStorage.setItem(timeBlock, task);
-
-}
-
-$('save1').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-
-$('save2').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-$('save3').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-$('save4').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-$('save5').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-
-$('save6').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-$('save7').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-$('save8').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-$('save9').on('click', function () {
-    saveTask('9am', $('text-entry1').text())
-})
-
-
-
-
+get_rows()
